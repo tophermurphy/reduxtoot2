53b222da2cb69bf04e4fb8a1097b9bb260698d5f
cmurphy <cmurphy@glunzbeers.com>
2018-02-19 13:06:34 -0600

    'work toot'

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '97a56da187134948d22a7a2a1e87b4f4';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATER';
+
+export function fetchWeather(city) {
+    const url =`${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
