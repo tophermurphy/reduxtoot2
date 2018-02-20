b11754dbeaa2b80018891fe53eeff0fca764393e
tophermurphy <tophermurphy@gmail.com>
2018-02-19 20:05:09 -0600

    'all the toots'

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '97a56da187134948d22a7a2a1e87b4f4';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATER';
 
